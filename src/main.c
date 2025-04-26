91054a5268778764636ba74d54682f5d92da3228
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-04-26 16:56:41 +0200

    Remove lexer from token struct

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "lexer/token.h"
 
 int main(void) {
-  char *code = "//3.5 * (5.75 + 4) \n 2 + 5";
+  char *code = "3.5 * (5.75 + 4)";
   lexer_t *lexer = tokenize(code);
   
   for(size_t i = 0 ; i < lexer->tokens->count; i++){
