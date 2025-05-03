d2f599cd33023b2686b9de6b94cff86db85ea486
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-05-03 17:39:14 +0200

    change some main staff

--- src/main.c
+++ src/main.c
@@ -3,15 +3,20 @@
 #include "utils/logger.h"
 
 int main(void) {
-  char *code = "3.5 * (5.75 + 4) \" bla bla bla \" >= , <= , == , ; , : , if , next";
+  char *code = "3.5 * (5.75 + 4) \" bla bla bla \" >= , blabla";
   dlog("Will parse %s \n" , code);
   lexer_t *lexer = tokenize(code);
   
+  dlog("Tokens list : ");
+  for(size_t i = 0 ; i < lexer->tokens->count; i++){
+      log_token((token_t*)lexer->tokens->items[i]);
+  }
+
   return 0;
 }
 
 /* 
-  for(size_t i = 0 ; i < lexer->tokens->count; i++){
-      log_token((token_t*)lexer->tokens->items[i]);
-  }
+ *
+ *
+ *
 */
